ca57f3acac6c2f6f9a02aeb2aa48df7f8424c86d
recipe-source <minntake3946@gmail.com>
2025-12-17 00:25:58 +0900

    design

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -8,22 +8,26 @@ class RecipesController < ApplicationController
 
   def show
         @recipe = Recipe.find_by(id: params[:id])
-    end
+        if @recipe.nil?
+        redirect_to recipes_path, alert: "指定されたレシピは見つかりませんでした。"
+        return
+        end
+  end
 
   def index
-  if params[:search].present?
+   if params[:search].present?
     @recipes = Recipe.where(
       "name LIKE ? OR material LIKE ? OR process LIKE ?",
       "%#{params[:search]}%",
       "%#{params[:search]}%",
       "%#{params[:search]}%"
     )
-  else
+   else
     @recipes = Recipe.all
+   end
   end
-end
 
-  def create
+ def create
     recipe = Recipe.new(recipe_params)
 
     recipe.user_id = current_user.id  
@@ -34,12 +38,17 @@ end
     else
        redirect_to :action => "new"
     end
-  end
-
-  private
-  def recipe_params
-    params.require(:recipe).permit(:name, :material, :process, :cost, :time, :image)
-  end
-
   
+
+   def destroy
+    @recipe = Recipe.find(params[:id])
+    @recipe.destroy
+    redirect_to recipes_path, notice: "レシピを削除しました。"
+   end
+   private
+   def recipe_params
+     params.require(:recipe).permit(:name, :material, :process, :cost, :time, :image)
+   end
+  end
+
 end
